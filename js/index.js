760adc71eaede14674d13ec9a5d2edfb3b5539fd
Humayun Kabir <kabirhumayun5902@gmail.com>
2023-05-05 02:36:03 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('.testimonial_slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+
+
+AOS.init({
+    offset: 250,
+    duration: 1000,
+});
